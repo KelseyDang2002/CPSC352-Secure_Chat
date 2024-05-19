32777b48cbfd1eb0603b3fecb15c90efa0c2a8c7
unknown <kchan21@csu.fullerton.edu>
2024-05-18 22:25:08 -0700

    created rsa encryption + decryption

--- user_keys/rsa_decryption.py
+++ user_keys/rsa_decryption.py
@@ -1,19 +1,19 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
+import os
 
-def rsa_decrypt_data(data, prKey):
-    cipher = PKCS1_v1_5.new(prKey)
-    plaintext = cipher.decrypt(data, 1000)
+def rsa_decrypt_data(data, prv_file):
+    cipher = PKCS1_v1_5.new(prv_file)
+    plaintext = cipher.decrypt(eval(data), 1000)
     print("Decrypted: ", plaintext)
 
 if __name__ == "__main__":
     data = input("Please enter the data you would like to decrypt: ")
     username = input("Please input your username: ")
     filename = "private_" + username + ".pem"
-    print(type(filename))
-    print (filename)
-    with open (filename, "rb") as pub_file:
-        contents = pub_file.read()
-        puKey = RSA.importKey(contents)
+    file_path = os.path.join("user_keys", filename)
+    with open (file_path, "rb") as prv_file:
+        contents = prv_file.read()
+        prv_file = RSA.importKey(contents)
 
-    rsa_decrypt_data(data, puKey)
+    rsa_decrypt_data(data, prv_file)
